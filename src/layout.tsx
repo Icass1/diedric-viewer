fd0f4ec6809a965d0f1deff9f2326089710ef5c6
IgnacioPCWSL <i.cassdolopez@gmail.com>
2024-09-19 11:07:59 +0200

    Modfiied layout for 2d canvas

--- src/layout.tsx
+++ src/layout.tsx
@@ -7,13 +7,17 @@ export default function Layout() {
     const canvasRef = useRef<HTMLCanvasElement>(null)
 
     return (
-        <div className=" h-full w-full grid grid-cols-[400px_1fr] gap-2">
+        <div className=" h-full w-full grid grid-cols-[400px_2fr_1fr] gap-2">
             <div className="bg-neutral-100 h-full w-full min-w-0 min-h-0">
                 <App canvasRef={canvasRef} />
             </div>
-            <div id="main-canvas" className="relative">
+            <div className="relative">
                 <canvas ref={canvasRef} className="absolute top-0 w-full min-w-0 h-full min-h-0"></canvas>
-                <div className="absolute flex flex-col p-1 m-1 bg-slate-600/50 rounded-lg text-sm" id="main-canvas-info"></div>
+                <div className="absolute flex flex-col p-1 m-1 bg-slate-600/50 rounded-lg text-sm"></div>
+            </div>
+            <div className="relative">
+                <canvas className="absolute top-0 w-full min-w-0 h-full min-h-0"></canvas>
+                <div className="absolute flex flex-col p-1 m-1 bg-slate-600/50 rounded-lg text-sm"></div>
             </div>
         </div>
     )
