a8cc2eb214c7a201204809f034322c41acbfe155
IgnacioPCWSL <i.cassdolopez@gmail.com>
2024-10-02 19:17:17 +0200

    Added intersections file

---
+++ src/utils/intersections.ts
@@ -0,0 +1,9 @@
+import { DiedricLine } from "./diedricLine";
+import { DiedricPlane } from "./diedricPlane";
+
+
+export function intersectionLinePlane(line: DiedricLine, plane: DiedricPlane) {
+
+
+    
+}
