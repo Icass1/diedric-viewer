ca15907feff9d9085ffe0fa8fdf321e5a91fd1ec
IgnacioPCWSL <i.cassdolopez@gmail.com>
2024-10-07 23:21:01 +0200

    Fixed some bugs in label

--- src/utils/two/label.ts
+++ src/utils/two/label.ts
@@ -1,19 +1,16 @@
 import { Vector2 } from "three"
 import { Canvas } from "./canvas"
-import { Point } from "./point"
 
 export class Label {
-    color: string
-    width: number
+    private color: string
     pos: Vector2
+    text: string
 
-
-    constructor({ text, color = "black", width = 1 }: { text: string, color?: string, width?: number }) {
+    constructor({ text, color = "black" }: { text: string, color?: string }) {
         this.color = color
-        this.width = width
 
         this.pos = new Vector2(0, 0)
-
+        this.text = text
 
 
     }
@@ -21,23 +18,17 @@ export class Label {
         if (this.pos?.x == undefined || this.pos?.y == undefined) return
 
         canvas.ctx.save();
-        canvas.ctx.translate(100, 100);
-        canvas.ctx.beginPath();
-        canvas.ctx.arc(
-            0,
-            0,
-            2, 0, 2 * Math.PI, false
+        canvas.ctx.translate(
+            this.pos.x * canvas.zoom + canvas.center.x,
+            this.pos.y * canvas.zoom + canvas.center.y - 3,
         );
-        canvas.ctx.fillStyle = this.color;
-        canvas.ctx.fill();
-        canvas.ctx.rotate(-Math.PI / 10 * 21);
-        canvas.ctx.fillStyle = "black"
-        canvas.ctx.font = "30px Arial";
+        canvas.ctx.fillStyle = this.color
+        canvas.ctx.font = "16px Arial";
         canvas.ctx.textAlign = "center"
         canvas.ctx.textBaseline = "bottom";
 
-        canvas.ctx.fillText("A''", 0, 0);
-
+        canvas.ctx.fillText(this.text, 0, 0);
         canvas.ctx.restore();
     }
+
 }
