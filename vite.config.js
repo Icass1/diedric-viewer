08c90a9729e4cde3a963e150ee1751daaae11798
IgnacioPCWSL <i.cassdolopez@gmail.com>
2024-10-10 22:25:26 +0200

    Added one file vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from "vite";
 import tailwindcss from "tailwindcss";
+import { viteSingleFile } from 'vite-plugin-singlefile';
+
 export default defineConfig({
-  plugins: [],
+  plugins: [viteSingleFile()],
   server: {
     host: "",
     port: 3000,
@@ -11,4 +13,8 @@ export default defineConfig({
       plugins: [tailwindcss()],
     },
   },
+  build: {
+    cssCodeSplit: false,  // Merge CSS into a single file
+    assetsInlineLimit: 100000000,  // Increase the limit to inline all assets
+  }
 });
