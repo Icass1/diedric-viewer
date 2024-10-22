b4f427786368ac3043e7fb8f28924d314b0e1624
IgnacioPCWSL <i.cassdolopez@gmail.com>
2024-10-22 19:25:50 +0200

    Added dashed to path

--- src/utils/two/path.ts
+++ src/utils/two/path.ts
@@ -5,10 +5,12 @@ export class Path {
     color: string
     width: number
     points: Vector2[] = []
+    dashed: boolean
 
-    constructor({ color = "black", width = 1 }: { color?: string, width?: number }) {
+    constructor({ color = "black", width = 1, dashed = false }: { color?: string, width?: number, dashed?: boolean }) {
         this.color = color
         this.width = width
+        this.dashed = dashed
 
     }
     render(canvas: Canvas) {
@@ -17,28 +19,29 @@ export class Path {
         canvas.ctx.save();
 
         canvas.ctx.lineWidth = this.width;
-
         canvas.ctx.strokeStyle = this.color
+        if (this.dashed) canvas.ctx.setLineDash([15, 5]);
+
         canvas.ctx.beginPath();
+
         canvas.ctx.moveTo(
             this.points[0].x * canvas.zoom + canvas.center.x,
             this.points[0].y * canvas.zoom + canvas.center.y
         );
 
         for (let i = 1; i < this.points.length; i++) {
-
             canvas.ctx.lineTo(
                 this.points[i].x * canvas.zoom + canvas.center.x,
                 this.points[i].y * canvas.zoom + canvas.center.y
             );
         }
 
-        canvas.ctx.lineTo(
-            this.points[0].x * canvas.zoom + canvas.center.x,
-            this.points[0].y * canvas.zoom + canvas.center.y
-        );
-        canvas.ctx.stroke();
+        // canvas.ctx.lineTo(
+        //     this.points[0].x * canvas.zoom + canvas.center.x,
+        //     this.points[0].y * canvas.zoom + canvas.center.y
+        // );
 
+        canvas.ctx.stroke();
         canvas.ctx.restore();
     }
 }
